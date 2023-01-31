4d0865b467a956e5ed7269f30cb2fbe4eee70b8d
0andme <yu00mi97@gmail.com>
2023-02-01 08:40:00 +0900

    230201 이벤트 캡쳐링

--- 230201/index.js
+++ 230201/index.js
@@ -1,5 +1,4 @@
-const buttonClick = (event) => {
-  // event 를 매개변수로 받음;
-  event.stopPropagation(); // 부모 요소로의 이벤트 버블링을 중지
-  alert("버튼을 눌렀습니다!");
-};
+let div = document.getElementById("div");
+div.addEventListener("click", () => alert("div"), true); // 이벤트 캡처링 true
+let button = document.getElementById("button");
+button.addEventListener("click", () => alert("button"));
