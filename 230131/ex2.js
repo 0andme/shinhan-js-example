073ae783907c241537920f1089c05cf926044105
0andme <yu00mi97@gmail.com>
2023-01-31 16:50:55 +0900

    fix : 불필요한 코드 삭제

--- 230131/ex2.js
+++ 230131/ex2.js
@@ -1,11 +1,3 @@
-// let seoulTime = new Date().toLocaleString("ko-KR", {timeZone: "Asia/Seoul"});
-// let londonTime = new Date().toLocaleString("ko-KR", {
-//   timeZone: "Europe/London",
-// });
-// let newYorkTime = new Date().toLocaleString("ko-KR", {
-//   timeZone: "America/New_York",
-// });
-
 let nowTime = document.getElementById("nowTime");
 let nowCity = "Asia/Seoul";
 
