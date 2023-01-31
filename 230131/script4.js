6436786fa69e6e8b63a50ca55b9bbd9f42ee18f8
0andme <yu00mi97@gmail.com>
2023-01-31 14:42:00 +0900

    feat : 230131

---
+++ 230131/script4.js
@@ -0,0 +1,34 @@
+{
+  /* <body>
+    <div id="firstDiv">
+      <p>안녕하세요</p>
+    </div>
+    <div id="secondDiv">안녕하세요</div>
+    <a href="https://www.naver.com"> 네이버로 가기 </a>
+    <input value="안녕" />
+  </body>
+  */
+}
+
+// let firstDiv = document.getElementById("firstDiv");
+// console.log(firstDiv.tagName); // div
+// console.log(firstDiv.innerHTML); //
+// firstDiv.innerText; // 안녕하세요
+// firstDiv.outerHTML;
+// // firstDiv.hidden = true;
+
+// let inputEl = document.getElementsByTagName("input")[0];
+
+// console.log(inputEl.value);
+// inputEl.value = "input 값 바꾸기";
+
+// firstDiv.style.backgroundColor = "blue";
+// firstDiv.style.color = "white";
+
+let blueDiv = document.getElementsByClassName("blue-div")[0];
+blueDiv.className = "green-div";
+
+blueDiv.classList.add("white-text");
+// blueDiv.classList.remove('white-text')
+// 클래스가 이미 있으면 제거, 없으면 추가
+blueDiv.classList.toggle("white-text");
