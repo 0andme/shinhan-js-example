3317c00fc814d6f1ead0a60021cc24d066c83bf8
0andme <yu00mi97@gmail.com>
2023-02-01 16:38:45 +0900

    fetch example

---
+++ 230201-2/ex_fetch.js
@@ -0,0 +1,41 @@
+let url = "https://jsonplaceholder.typicode.com/";
+
+async function getPosts() {
+  let res = await fetch(`${url}posts`);
+  let data = await res.json();
+  return data;
+}
+async function deletePost(event) {
+  let res = await fetch(`${url}posts/${event.target.id}`, {
+    method: "DELETE",
+  });
+  let data = await res.json();
+  return data;
+}
+
+async function insertPostsList() {
+  let posts = await getPosts();
+  console.log(posts);
+
+  let listEl = document.getElementById("postList");
+
+  for (let post of posts) {
+    let liEl = document.createElement("div");
+    let titleEl = document.createElement("h4");
+    let bodyEl = document.createElement("p");
+    let deleteBtnEl = document.createElement("button");
+
+    titleEl.innerText = post.title;
+    bodyEl.innerText = post.body;
+    deleteBtnEl.innerText = "삭제하기";
+    deleteBtnEl.setAttribute("id", post.id);
+    deleteBtnEl.addEventListener("click", deletePost);
+
+    liEl.append(titleEl);
+    liEl.append(bodyEl);
+    liEl.append(deleteBtnEl);
+    listEl.append(liEl);
+  }
+}
+
+insertPostsList();
